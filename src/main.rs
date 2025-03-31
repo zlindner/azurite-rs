4908ea91eb46711bb627718d1f928af280da9c3a
zlindner <zach.lindner@hotmail.com>
2025-03-31 18:39:14 -0400

    Basic axum http server

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{Router, routing::get};
+use tokio::net::TcpListener;
+
+#[tokio::main]
+async fn main() {
+    let app = Router::new().route("/", get(|| async { "Hello, World!" }));
+    let listener = TcpListener::bind("0.0.0.0:1337").await.unwrap();
+    axum::serve(listener, app).await.unwrap();
 }
